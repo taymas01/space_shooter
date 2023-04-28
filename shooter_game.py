a604007d1d65fabcf0b414f70211b771bf3dfc7c
taymas01 <132080261+taymas01@users.noreply.github.com>
2023-04-28 20:00:08 +0500

    Update shooter_game.py
    version 1.1

--- shooter_game.py
+++ shooter_game.py
@@ -25,3 +25,35 @@ class Gamesprite(sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         self.direction = 'l'
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(Gamesprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 1:
+            self.rect.x -= self.speed
+        if keys[K_a] and self.rect.x >1:
+            self.rect.x -= self.speed
+
+        if keys[K_RIGHT] and self.rect.x < 630:
+            self.rect.x += self.speed
+        if keys[K_d] and self.rect.x < 630:
+            self.rect.x += self.speed
+
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+        if keys[K_DOWN] and self.rect.y < 435:
+            self.rect.y += self.speed
+        if keys[K_s] and self.rect.y <435:
+            self.rect.y += self.speed
+
+    def fire(self):
+        bullet = Bullet('bullet.png', self.rect.centerx-10, self.rect.top, 20, 50, 15)
+        bullets.add(bullet)                                                                                                                 
+
+lost = 0
