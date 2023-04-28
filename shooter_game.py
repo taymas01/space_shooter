225733a5545510426a659c77a8be4b4cc79f5342
taymas01 <132080261+taymas01@users.noreply.github.com>
2023-04-28 19:58:03 +0500

    Create shooter_game.py
    1.0

---
+++ shooter_game.py
@@ -0,0 +1,27 @@
+from pygame import *
+from random import *
+from time import time as timer
+
+window = display.set_mode((700, 500))
+display.set_caption('Шутер')
+
+background = transform.scale(image.load('space3.jpg'), (700, 500))
+clock = time.Clock()
+FPS = 60
+
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.set_volume(0.2)
+mixer.music.play()
+
+fire = mixer.Sound('fire.ogg')
+
+class Gamesprite(sprite.Sprite):
+    def __init__(self, player_image, x, y, widht, height, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (widht, height))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.direction = 'l'
